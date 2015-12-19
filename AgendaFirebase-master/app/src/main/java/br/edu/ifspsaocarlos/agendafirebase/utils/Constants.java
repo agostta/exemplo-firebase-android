240244210c48d4d4f6f668a4ba56f6d1004121ae
unknown <agostta@SILVAHUG6.americas.hpqcorp.net>
2015-12-19 17:13:30 -0200

    first commit

---
+++ AgendaFirebase-master/app/src/main/java/br/edu/ifspsaocarlos/agendafirebase/utils/Constants.java
@@ -0,0 +1,12 @@
+package br.edu.ifspsaocarlos.agendafirebase.utils;
+
+
+
+public final class Constants {
+
+    //https://burning-heat-6905.firebaseio.com/#-K5luaXbeXb_JPRP97Rf|f30b771e29e7e33b1934c7e5395f153b
+    public static final String FIREBASE_URL="https://burning-heat-6905.firebaseio.com/";
+
+
+
+}
